4e846432a891d6a87b56f5332a3c8cf8c2a027c0
Saud Punjwani <saudpunjwani101@gmail.com>
2017-07-30 00:19:30 +0500

    Update Accounts.js

--- Accounts.js
+++ Accounts.js
@@ -1,7 +1,10 @@
+//now importing the base class where the crud methods are called for every table
 import Base from './base';
 
+//inherting all the base methods
 class Accounts extends Base {
   constructor() {
+      //calling the base class constructor and passing Account table, so the data will be created in the account table
       super('Account');
   }
 }
